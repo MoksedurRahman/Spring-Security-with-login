5703aa6ec30dde59aad9a9d01be8916c111bfdb4
Md Moksedur Rahman <moksedur.rahman@gmail.com>
2022-04-10 21:01:57 +0600

    init

---
+++ src/test/java/com/datavisionbd/springsecurity/SpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.datavisionbd.springsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
