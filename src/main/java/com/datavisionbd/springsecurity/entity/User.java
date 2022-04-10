aa53369a71b035dea09a8b13add21e03ac0f4821
Md Moksedur Rahman <moksedur.rahman@gmail.com>
2022-04-10 21:59:05 +0600

    role added

---
+++ src/main/java/com/datavisionbd/springsecurity/entity/User.java
@@ -0,0 +1,22 @@
+package com.datavisionbd.springsecurity.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User  {
+
+    @Id
+    private Long Id;
+    private String username;
+    private String password;
+    private String role;
+
+}
---
+++ src/main/java/com/datavisionbd/springsecurity/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.datavisionbd.springsecurity.repository;
+
+import com.datavisionbd.springsecurity.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,Long> {
+
+    User findByUsername(String username);
+}
